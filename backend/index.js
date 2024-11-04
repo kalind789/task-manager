ed9b6c459c621a247474441a530ebb2a31a91c96
kadhika2 <kadhika2@u.rochester.edu>
2024-11-04 16:03:09 -0500

    Starting on the frontend. Created two srparated folders.

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 // define a port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const tasksRoute = require('./routes/tasks');
 app.use('/tasks', tasksRoute);
