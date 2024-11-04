21211fe8950ca9bd5b19b037669fdf9fa44f50fa
kadhika2 <kadhika2@u.rochester.edu>
2024-11-04 08:33:03 -0500

    Updated tasks with basic delete, put, post.

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 // define a sample route
-router.get('/',(req, res) => { res.send('Task manager home!'); });
+router.get('/', (req, res) => { 
+    res.send('Task manager home!'); 
+});
+
+router.post('/', (req, res) => {
+    res.send('Create a new task');
+});
+
+router.put('/:id', (req, res) => {
+    res.send(`Update task with ID ${req.params.id}`);
+});
+
+router.delete('/:id', (req, res) => {
+    res.send(`Delete task with ID ${req.params.id}`);
+});
 
 module.exports = router;
